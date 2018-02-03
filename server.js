3ff14bfad9bd136a07195b7756737b82eec5815d
FravashY <fravashy@gmail.com>
2018-02-04 01:12:50 +0300

    Added projects page.

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.get('/bad', (req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    welcomeMessage: 'Here are my awesome projects!'
+  })
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 });
